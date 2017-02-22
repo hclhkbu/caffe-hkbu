69c4f57ab8b989940e99047276273e18e2203d42
shyhuai <shaohuaishi@gmail.com>
2017-02-22 22:30:46 +0800

    add 2 layers

---
+++ benchmarks/benchmark.py
@@ -0,0 +1,44 @@
+import argparse
+import sys,os,time
+import subprocess
+import re
+
+#caffebin='/home/dl/caffe-hkbu-lr/build-8.0/tools/caffe'
+caffebin='/home/dl/caffe-openblas/build/tools/caffe'
+config_file_home='/home/dl/caffe-hkbu-lr/benchmarks/2_layer'
+#default_gpu_id=0
+default_gpu_id=1
+
+def get_average_time(filename):
+    file = open(filename, "r")
+    search_str = 'Average Forward-Backward: '
+    for line in file.readlines():
+        if re.search(search_str, line):
+            start_idx = line.find(search_str)+len(search_str)
+            end_idx = line.find('ms.')
+            ms = float(line[start_idx:end_idx].strip())
+            return ms
+    return 0 
+
+
+def execute(config_file):
+    logfile = '%s.log'%config_file
+    cmd = '%s time -model=%s/%s -gpu=%d -iterations=16>&%s'%(caffebin, config_file_home, config_file, default_gpu_id, logfile)
+    #print cmd
+    #os.system(cmd)
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+    process.wait()
+    ms = get_average_time(logfile)
+    return ms
+
+if __name__ == '__main__':
+    #hiddens = [[4096, 4096], [128, 1024]]
+    hiddens = [[4096, 4096]]
+    batches = [128, 256, 512, 1024, 2048]
+    for hidden in hiddens:
+        h1 = hidden[0]
+        h2 = hidden[1]
+        for batch in batches:
+            config_file = '%d-%d-b%d.prototxt' % (h1, h2, batch)
+            ms = execute(config_file)
+            print ms/1000
